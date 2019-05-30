687b65a5d0aab163d7281fc62dbee8ee0f0c07b3
cassio <cassio@GTI-Cassio.crea-mg.com.br>
2019-05-30 17:55:05 -0300

    Primeiro commit AulaGuitJava

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,12 @@
+
+*
+ * O primeiro programa em Java: Hello World
+ * Autor: Cássio Alves
+ */
+
+// Todo programa tem um ponto de entrada: o "método" main de alguma "classe"
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
